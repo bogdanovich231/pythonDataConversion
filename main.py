92a34b71b471ce7c1b9eead3df3c53bbb5a08eb4
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:45:04 +0200

    feat: adding UI interface and async convert

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import json
 import xml.etree.ElementTree as ET
 import yaml
+import asyncio
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFileDialog, QMessageBox
+
 def convertFile(inputFile, outputFile):
     _, extension = os.path.splitext(inputFile)
 
@@ -17,7 +20,7 @@ def convertFile(inputFile, outputFile):
         with open(inputFile, 'r') as yamlFile:
             data = yaml.safe_load(yamlFile)
     else:
-        print("Unsupported file format.")
+        QMessageBox.critical(None, "Unsupported file format", "Unsupported file format.")
         return
 
     _, outputExtension = os.path.splitext(outputFile)
@@ -33,11 +36,10 @@ def convertFile(inputFile, outputFile):
         with open(outputFile, 'w') as yamlOutput:
             yaml.dump(data, yamlOutput, default_flow_style=False)
     else:
-        print("Unsupported output file format.")
+        QMessageBox.critical(None, "Unsupported output file format", "Unsupported output file format.")
         return
 
-    print("The data conversion is complete.")
-
+    QMessageBox.information(None, "Data conversion complete", "The data conversion is complete.")
 
 def xml_to_dict(element):
     if len(element) == 0:
@@ -54,7 +56,6 @@ def xml_to_dict(element):
             result[child.tag] = child_data
     return result
 
-
 def dict_to_xml(data):
     root = None
     if isinstance(data, dict):
@@ -68,14 +69,57 @@ def dict_to_xml(data):
         root.text = str(data)
     return root
 
+async def processFiles():
+    app = QApplication(sys.argv)
+    widget = QWidget()
+    layout = QVBoxLayout()
+
+    label = QLabel("Select input and output files:")
+    layout.addWidget(label)
+
+    hbox = QHBoxLayout()
+    inputButton = QPushButton("Select Input File")
+    outputButton = QPushButton("Select Output File")
+    hbox.addWidget(inputButton)
+    hbox.addWidget(outputButton)
+    layout.addLayout(hbox)
+
+    convertButton = QPushButton("Convert")
+    layout.addWidget(convertButton)
+
+    widget.setLayout(layout)
+
+    input_file = None
+    output_file = None
+
+    def selectInputFile():
+        nonlocal input_file
+        input_file, _ = QFileDialog.getOpenFileName(None, "Select Input File")
+        if input_file:
+            inputButton.setText(os.path.basename(input_file))
+
+    def selectOutputFile():
+        nonlocal output_file
+        output_file, _ = QFileDialog.getSaveFileName(None, "Select Output File")
+        if output_file:
+            outputButton.setText(os.path.basename(output_file))
+
+    def convert():
+        nonlocal input_file, output_file
+        if input_file and output_file:
+            asyncio.create_task(convertFile(input_file, output_file))
+
+    inputButton.clicked.connect(selectInputFile)
+    outputButton.clicked.connect(selectOutputFile)
+    convertButton.clicked.connect(convert)
+
+    widget.show()
+    sys.exit(await app.exec_())
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("Использование: program.exe pathFile1.x pathFile2.y")
-        print("где x и y - один из форматов .xml, .json и .yml (.yaml).")
-        sys.exit(1)
-
-    inputFile = sys.argv[1]
-    outputFile = sys.argv[2]
-
-    convertFile(inputFile, outputFile)
+        asyncio.run(processFiles())
+    else:
+        inputFile = sys.argv[1]
+        outputFile = sys.argv[2]
+        asyncio.run(convertFile(inputFile, outputFile))
