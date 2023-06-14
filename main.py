5d6cf22338bf9918a56aa8fe81c13d372552831e
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:48:12 +0200

    feat: create import async

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 import yaml
 import asyncio
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFileDialog, QMessageBox
-
+import asyncio.subprocess
 def convertFile(inputFile, outputFile):
     _, extension = os.path.splitext(inputFile)
 
