dbec777b3a5b38d464559f42b9b6af37e6e51cce
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:52:21 +0200

    feat: adding import

--- installResources.py
+++ installResources.py
@@ -3,3 +3,6 @@ import os
 import json
 import xml.etree.ElementTree as ET
 import yaml
+import asyncio
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFileDialog, QMessageBox
+import asyncio.subprocess
