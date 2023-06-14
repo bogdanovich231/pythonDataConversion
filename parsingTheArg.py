2d286bbded198bc51ee1806180eec63b0f79fb9a
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:53:26 +0200

    feat: update parsing args

--- parsingTheArg.py
+++ parsingTheArg.py
@@ -1,10 +1,7 @@
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("Using: main.py pathFile1.x pathFile2.y")
-        print("where x and y are one of the .xml, .json, and .yml (.yaml) formats.")
-        sys.exit(1)
-
-    inputFile = sys.argv[1]
-    outputFile = sys.argv[2]
-
-    convertFile(inputFile, outputFile)
+        asyncio.run(processFiles())
+    else:
+        inputFile = sys.argv[1]
+        outputFile = sys.argv[2]
+        asyncio.run(convertFile(inputFile, outputFile))
