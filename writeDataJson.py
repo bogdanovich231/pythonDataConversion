6890041cabdde5746de98529af2bbd59f413abb6
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:56:34 +0200

    feat: feat: adding ui interface

--- writeDataJson.py
+++ writeDataJson.py
@@ -5,14 +5,14 @@ def convertFile(inputFile, outputFile):
         tree = ET.parse(inputFile)
         root = tree.getroot()
         data = xml_to_dict(root)
-    elif extension == '.json':
-        with open(inputFile, 'r') as jsonFile:
-            data = json.load(jsonFile)
+    # elif extension == '.json':
+    #     with open(inputFile, 'r') as jsonFile:
+    #         data = json.load(jsonFile)
     elif extension == '.yml' or extension == '.yaml':
         with open(inputFile, 'r') as yamlFile:
             data = yaml.safe_load(yamlFile)
     else:
-        print("Unsupported file format.")
+        QMessageBox.critical(None, "Unsupported file format", "Unsupported file format.")
         return
 
     _, outputExtension = os.path.splitext(outputFile)
@@ -28,10 +28,11 @@ def convertFile(inputFile, outputFile):
         with open(outputFile, 'w') as yamlOutput:
             yaml.dump(data, yamlOutput, default_flow_style=False)
     else:
-        print("Unsupported output file format.")
+        QMessageBox.critical(None, "Unsupported output file format", "Unsupported output file format.")
         return
 
-    print("The data conversion is complete.")
+    QMessageBox.information(None, "Data conversion complete", "The data conversion is complete.")
+
 
 
 # write data from an object to a file in the format and according to the syntax of the .json file.
