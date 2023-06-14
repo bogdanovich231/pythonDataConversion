a3d0ce7da2374a6c92509c427a930d530da719dd
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-06-14 13:16:56 +0200

    Create saveDataYml.py

---
+++ saveDataYml.py
@@ -0,0 +1,39 @@
+def convertFile(inputFile, outputFile):
+    _, extension = os.path.splitext(inputFile)
+
+    if extension == '.xml':
+        tree = ET.parse(inputFile)
+        root = tree.getroot()
+        data = xml_to_dict(root)
+    elif extension == '.json':
+        with open(inputFile, 'r') as jsonFile:
+            data = json.load(jsonFile)
+    elif extension == '.yml' or extension == '.yaml':
+        with open(inputFile, 'r') as yamlFile:
+            data = yaml.safe_load(yamlFile)
+    else:
+        print("Unsupported file format.")
+        return
+
+    _, outputExtension = os.path.splitext(outputFile)
+
+    if outputExtension == '.xml':
+        root = dict_to_xml(data)
+        tree = ET.ElementTree(root)
+        tree.write(outputFile, encoding='utf-8', xml_declaration=True)
+    elif outputExtension == '.json':
+        with open(outputFile, 'w') as jsonOutput:
+            json.dump(data, jsonOutput, indent=4)
+    elif outputExtension == '.yml' or outputExtension == '.yaml':
+        with open(outputFile, 'w') as yamlOutput:
+            yaml.dump(data, yamlOutput, default_flow_style=False)
+    else:
+        print("Unsupported output file format.")
+        return
+
+    print("The data conversion is complete.")
+
+# Task 5: save data from an object to a file in the format and according to the syntax of the .yml file.
+    # elif outputExtension == '.yml' or outputExtension == '.yaml':
+    #     with open(outputFile, 'w') as yamlOutput:
+    #         yaml.dump(data, yamlOutput, default_flow_style=False)
